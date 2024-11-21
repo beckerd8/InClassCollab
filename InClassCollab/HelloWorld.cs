63d4eba58cae4ef34ede710f41f41cbc741f16df
slivinmb <slivinmb@LCBLAB2110-27>
2024-11-21 11:07:49 -0500

    Add project files.

---
+++ InClassCollab/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InClassCollab
+{
+    public class HelloWorld
+    {
+        //Each person has their own class
+    }
+}
