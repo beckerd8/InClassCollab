62ee47f65aa73719e65ca8259c3e09f0a71f7055
beckerd8 <beckerd8@mail.uc.edu>
2024-11-21 11:14:07 -0500

    You are gonna have to smell like cheese after this!

--- InClassCollab/HelloWorld.cs
+++ InClassCollab/HelloWorld.cs
@@ -8,5 +8,9 @@ namespace InClassCollab
     public class HelloWorld
     {
         //Each person has their own class
+        public static void SayHello() 
+        {
+            Console.WriteLine("Micheal Slivinski smells like cheese and he has to merge this!()");
+        }
     }
 }
